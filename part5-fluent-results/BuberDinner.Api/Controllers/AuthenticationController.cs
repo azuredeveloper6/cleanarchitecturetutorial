e3d345a0b620e5ad6c43393d4d120753a7145709
Santhanam Pitchai <azuredeveloper6@gmail.com>
2023-11-25 21:04:45 -0500

    part 5 tutorial

---
+++ part5-fluent-results/BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,72 @@
+using BuberDinner.Application.Common.Errors;
+using BuberDinner.Application.Services.Authentication;
+using BuberDinner.Contracts.Authentication;
+using FluentResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers;
+
+[ApiController]
+[Route("auth")]
+public class AuthenticationCOntroller : ControllerBase
+{
+
+    private readonly IAuthenticationService _authenticationService;
+
+    public AuthenticationCOntroller(IAuthenticationService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+
+
+    [HttpPost("register")]
+    public IActionResult Register(RegisterRequest request)
+    {
+        Result<AuthenticationResult> registerResult = _authenticationService.Register(
+            request.FirstName,
+            request.LastName,
+            request.Email,
+            request.Password);
+
+        if (registerResult.IsSuccess)
+        {
+            return Ok(MapAuthResult(registerResult.Value));
+        }
+
+        var firstError = registerResult.Errors[0];
+        if (firstError is DuplicateEmailError)
+        {
+            return Problem(statusCode: StatusCodes.Status409Conflict, detail: "Email already exists from fluent - detail");
+        }
+
+        return Problem();
+    }
+
+    private static AuthenticationResponse MapAuthResult(AuthenticationResult authResult)
+    {
+        return new AuthenticationResponse(
+            authResult.User.Id,
+            authResult.User.FirstName,
+            authResult.User.LastName,
+            authResult.User.Email,
+            authResult.Token);
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginRequest request)
+    {
+        var authResult = _authenticationService.Login(request.Email, request.Password);
+
+        var response = new AuthenticationResponse(
+            authResult.User.Id,
+            authResult.User.FirstName,
+            authResult.User.LastName,
+            authResult.User.Email,
+            authResult.Token
+        );
+
+        return Ok(response);
+    }
+
+
+}
